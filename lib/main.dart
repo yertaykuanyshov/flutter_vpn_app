733028996a3f88caed47d0abf95bf19100ef7565
ertai <ertai.master@gmail.com>
2023-05-12 01:46:35 +0500

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:kalkan/bloc/vpn_bloc.dart';
 import 'package:kalkan/services/vpn_service.dart';
 
 import 'app.dart';
+import 'blocs/vpn_bloc.dart';
 
 void main() {
   final vpnService = VpnServiceImpl();
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:kalkan/bloc/vpn_bloc.dart';
 import 'package:kalkan/ext.dart';
-
-import '../config.dart';
+import '../blocs/vpn_bloc.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
